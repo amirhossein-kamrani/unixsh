0d5a43df26144b19137f4b5f9369e0959ed91640
amirhossein-kamrani <amir.h.k.n1384@gmail.com>
2025-11-24 18:44:11 +0330

    parse input

--- main.c
+++ main.c
@@ -5,6 +5,25 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+char **parse_input(char *input) {
+  int Index = 1;
+  char **Tokens = malloc(1024 * sizeof(char *));
+  char *Token;
+  Token = strtok(input, " \n");
+  Tokens[0] = Token;
+
+  while (Token != NULL)
+  {
+    Token = strtok(NULL, " ");
+    Tokens[Index] = Token;
+    Index++;
+  }
+
+  Tokens[Index] = NULL;
+  
+  return Tokens;
+}
+
 int main() {
 
   while (true)
@@ -13,7 +32,11 @@ int main() {
     char *UserInput;
     UserInput = readline("unixsh> ");
     add_history(UserInput);
-    printf("%s\n", UserInput);
+
+    char **Arguments = parse_input(UserInput);
+    char Commands = Arguments[0];
+
+    free(UserInput);
     
   }
 
